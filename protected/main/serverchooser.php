01b754be61d56cd532e57364cfad9e7157ce647a
Alexander Popov <alexandpopov@yandex.ru>
2021-05-29 17:35:21 +0300

    Update serverchooser.php

--- protected/main/serverchooser.php
+++ protected/main/serverchooser.php
@@ -1,5 +1,5 @@
 <?php
-class serverchooser{
+class serverchooser{//Первоначально материалы грузились на домашние серверы. Надо было проверять, какой из них работает.
     public static function choose($case='lectures'){
         include('protected/includes/servers.php');
 $waitTimeoutInSeconds = 1; 
